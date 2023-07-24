0f258cca2d4db87f2f61a6aefad1b026cbf5ba1b
Remberto Villar <rembertovillar@gmail.com>
2023-07-24 17:57:01 -0400

    add Benefits component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import Navbar from "@/scenes/navbar"
+import Home from "@/scenes/home"
+import Benefits from "@/scenes/benefits"
 import { useEffect, useState } from "react"
-import Navbar from "./scenes/navbar"
 import { SelectedPage } from "@/shared/types"
-import Home from "./scenes/home"
 
 function App() {
 	const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
@@ -24,6 +25,7 @@ function App() {
 		<div className='app bg-gray-20'>
 			<Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
 			<Home setSelectedPage={setSelectedPage} />
+			<Benefits setSelectedPage={setSelectedPage} />
 		</div>
 	)
 }
